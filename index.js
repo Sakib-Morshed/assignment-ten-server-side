e2d0b3c155c6c0d1a20a84fbcca06bd4acf5f1db
sakib <sakibmorshed69@gmailcom>
2025-11-12 02:51:01 +0600

    create post request

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+const uri =
+  "mongodb+srv://habitTracker:7HVB4FTBegcbYMNr@cluster0.myy7mcl.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("smart server is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("habitTracker");
+    const modelCollection = db.collection("allHabits");
+
+    app.get("/allHabits", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    app.post("/allHabits", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await modelCollection.insertOne(data);
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.listen(port, () => {
+  console.log(`smart server is running on port ${port}`);
+});
